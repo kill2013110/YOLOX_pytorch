3ee6f8862468755294d5ae07b33f18e7e345be17
kill2013110 <2803134162@qq.com>
2023-03-14 10:31:34 +0800

    ignore

--- temp.py
+++
@@ -1,4 +0,0 @@
-import torch
-a = torch.randn([1,8,3,3])
-b = a.chunk(4, dim=1)
-print('asd')
--- yolox/reserach.py
+++
@@ -1,5 +0,0 @@
-import torch, copy
-from thop import profile
-import torchvision
-
-net = torchvision.models.efficientnet_v2_s
